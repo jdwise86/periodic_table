eafd4a22bab029e61ff93080292a0679439203ab
jdwise86 <jdwise86@gmail.com>
2025-02-16 19:22:02 +0000

    feat: periodic_table.sql and element.sh
    feat: periodic_table.sql table properties remove trailing zeros
    
    feat: Update element.sh INSERT
    
    feat: Update element.sh Inserts
    
    feat: Update element.sh

--- element.sh
+++ element.sh
@@ -7,32 +7,59 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo -e "\n~~~~~ periodic_table ~~~~~\n"
 
 MAIN_MENU() {
-  if [[ $1 ]]
-  then
+  if [[ $1 ]]; then
     echo -e "\n$1"
   fi
 
   echo "Please provide an element as an argument."
   read ELEMENT_NAME
 
-  # Get element name
-  ELEMENT=$($PSQL "SELECT name FROM elements WHERE name = '$ELEMENT_NAME';")
+  # Get element data
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+                   FROM elements 
+                   JOIN properties USING(atomic_number) 
+                   JOIN types USING(type_id) 
+                   WHERE atomic_number::TEXT = '$ELEMENT_NAME' 
+                   OR symbol = '$ELEMENT_NAME' 
+                   OR name = '$ELEMENT_NAME';")
 
   # Check if element exists
-  if [[ -z $ELEMENT ]]
-  then
-    # Get new element name
-    echo -e "\nI don't have a record for that element, what's the symbol for this instrument?"
+  if [[ -z $ELEMENT ]]; then
+    echo -e "\nI don't have a record for that element, what's the symbol for this element?"
     read SYMBOL_NAME
+    
+    echo -e "\nWhat is its atomic number?"
+    read ATOMIC_NUMBER
+
+    echo -e "\nWhat is its atomic mass?"
+    read ATOMIC_MASS
+
+    echo -e "\nWhat is its melting point (°C)?"
+    read MELTING_POINT
+
+    echo -e "\nWhat is its boiling point (°C)?"
+    read BOILING_POINT
+
+    echo -e "\nWhat is its metal type?"
+    read METAL_TYPE
+
+    # Get Metal Type ID
+    METAL_TYPE_ID=$($PSQL "SELECT type_id FROM types WHERE type = '$METAL_TYPE';")
+
     # Insert new element
-    INSERT_SYMBOL_RESULT=$($PSQL "INSERT INTO elements(name, symbol) VALUES('$ELEMENT_NAME', '$SYMBOL_NAME')")
+    INSERT_SYMBOL_RESULT=$($PSQL "INSERT INTO elements(atomic_number, name, symbol) VALUES('$ATOMIC_NUMBER', '$ELEMENT_NAME', '$SYMBOL_NAME');")
+
+    # Insert element properties
+    INSERT_PROPERTIES_RESULT=$($PSQL "INSERT INTO properties(atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id) 
+                                      VALUES('$ATOMIC_NUMBER', '$ATOMIC_MASS', '$MELTING_POINT', '$BOILING_POINT', '$METAL_TYPE_ID');")
   fi
 
-  # Get symbol ID
-  SYMBOL_ID=$($PSQL "SELECT symbol FROM elements WHERE name='$ELEMENT_NAME'")
+  # Parse the query result into variables
+  IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME METAL_TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT"
 
   # Display element information
-  echo -e "\nThe element with atomic number 1 is $ELEMENT_NAME ($SYMBOL_ID). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $METAL_TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT°C and a boiling point of $BOILING_POINT°C."
 }
 
-MAIN_MENU  # Call the function
+# Call the function
+MAIN_MENU
