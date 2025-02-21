22e581edea9f7cf516396f31110f00b87e40ed62
jdwise86 <jdwise86@gmail.com>
2025-02-21 03:00:45 +0000

    fix: element.sh
    chore: update discrepancies
    
    fix C* to celsius element.sh
    
    fix element.sh I could not find that element in the database.

--- element.sh
+++ element.sh
@@ -3,86 +3,69 @@
 # Define the PSQL variable for database connection
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# Display the welcome message
-echo -e "\n~~~~~ periodic_table ~~~~~\n"
-
-MAIN_MENU() {
-  if [[ $1 ]]; then
-    echo -e "\n$1"
-  fi
-
+# First check that we got an argument, if not print the specified message and exit.
+if [[ -z $1 ]];then
   echo "Please provide an element as an argument."
-  read INPUT
+  exit 0
+fi
 
-  PRINT_ELEMENT "$INPUT"
-}
-
-PRINT_ELEMENT() {
-  INPUT=$1
-  if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
-    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol='$INPUT' OR name='$INPUT';") | sed 's/ //g')
-  else
-    ATOMIC_NUMBER=$INPUT
-  fi
 
   # Get element data
   ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
                    FROM elements 
                    JOIN properties USING(atomic_number) 
                    JOIN types USING(type_id) 
-                   WHERE atomic_number::TEXT = '$ATOMIC_NUMBER' 
-                   OR symbol = '$INPUT' 
-                   OR name = '$INPUT';")
+                   WHERE atomic_number::TEXT = '$1' 
+                   OR symbol = '$1' 
+                   OR name = '$1';")
 
   # Check if element exists
   if [[ -z $ELEMENT ]]; then
-    echo -e "\nI don't have a record for that element."
+  echo "I could not find that element in the database."
+  exit 0
+  fi
+   # echo -e "\nwhat's the symbol for this element?"
+   # read SYMBOL
 
-    echo -e "\nwhat's the symbol for this element?"
-    read SYMBOL
+   # echo -e "\nWhat is its name?"
+   # read NAME
 
-    echo -e "\nWhat is its name?"
-    read NAME
+   # echo -e "\nWhat is its atomic number?"
+   # read NUMBER
 
-    echo -e "\nWhat is its atomic number?"
-    read ATOMIC_NUMBER
+   # echo -e "\nWhat is its atomic mass?"
+   # read ATOMIC_MASS
 
-    echo -e "\nWhat is its atomic mass?"
-    read ATOMIC_MASS
+   # echo -e "\nWhat is its melting point (°C)?"
+   # read MELTING_POINT_C
 
-    echo -e "\nWhat is its melting point (°C)?"
-    read MELTING_POINT
+   # echo -e "\nWhat is its boiling point (°C)?"
+   # read BOILING_POINT_C
 
-    echo -e "\nWhat is its boiling point (°C)?"
-    read BOILING_POINT
-
-    echo -e "\nWhat is its metal type?"
-    read METAL_TYPE
+   # echo -e "\nWhat is its metal type?"
+   # read TYPE
 
     # Get Metal Type ID
-    METAL_TYPE_ID=$($PSQL "SELECT type_id FROM types WHERE type = '$METAL_TYPE';")
+    #TYPE_ID=$($PSQL "SELECT type_id FROM types WHERE type = '$TYPE';")
 
     # Insert new element
-    INSERT_SYMBOL_RESULT=$($PSQL "INSERT INTO elements(atomic_number, name, symbol) VALUES('$ATOMIC_NUMBER', '$NAME', '$SYMBOL');")
+    #INSERT_SYMBOL_RESULT=$($PSQL "INSERT INTO elements(atomic_number, name, symbol) VALUES('$NUMBER', '$NAME', '$SYMBOL');")
 
     # Insert element properties
-    INSERT_PROPERTIES_RESULT=$($PSQL "INSERT INTO properties(atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id) 
-                                      VALUES('$ATOMIC_NUMBER', '$ATOMIC_MASS', '$MELTING_POINT', '$BOILING_POINT', '$METAL_TYPE_ID');")
+    #INSERT_PROPERTIES_RESULT=$($PSQL "INSERT INTO properties(atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id) 
+                                      #VALUES('$NUMBER', '$ATOMIC_MASS', '$MELTING_POINT_C', '$BOILING_POINT_C', '$TYPE_ID');")
   
      # **Fetch the newly inserted element data*
-  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
-                     FROM elements 
-                     JOIN properties USING(atomic_number) 
-                     JOIN types USING(type_id) 
-                     WHERE atomic_number = '$ATOMIC_NUMBER';")
-  fi
+ # ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+ #                    FROM elements 
+ #                    JOIN properties USING(atomic_number) 
+ #                    JOIN types USING(type_id) 
+ #                    WHERE atomic_number = '$NUMBER';")
+
 
   # Parse the query result into variables
-  IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME METAL_TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT"
+  IFS="|" read -r NUMBER SYMBOL NAME TYPE ATOMIC_MASS MELTING_POINT_C BOILING_POINT_C <<< "$ELEMENT"
 
   # Display element information
-  echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $METAL_TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT°C and a boiling point of $BOILING_POINT°C."
-}
+  echo -e "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_C celsius and a boiling point of $BOILING_POINT_C celsius."
 
-# Call the function
-MAIN_MENU
